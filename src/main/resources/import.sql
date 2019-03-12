8f7e0938ec370554a4dfe0c112022980695fdee1
Brayan-Restrepo <brayan2293@gmail.com>
2019-03-12 00:50:26 -0500

    import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,7 +6,16 @@ INSERT INTO TARJETAS (id, cliente_id, numero, tipo, ccv) VALUES (1, 1, '1234-985
 INSERT INTO TARJETAS (id, cliente_id, numero, tipo, ccv) VALUES (2, 1, '2368-9853-8654-5425', 'B', '9633');
 INSERT INTO TARJETAS (id, cliente_id, numero, tipo, ccv) VALUES (3, 2, '8534-7396-7434-1395', 'C', '3233');
 
-INSERT INTO CONSUMOS (id, tarjeta_id, descripcion, fecha_consumo, monto) VALUES (1, 1, 'Compras de pc', '2019-03-09', '1233');
+INSERT INTO CONSUMOS (id, tarjeta_id, descripcion, fecha_consumo, monto) VALUES (1, 1, 'Compras de pc', '2019-03-09', '$1.100.233');
+INSERT INTO CONSUMOS (id, tarjeta_id, descripcion, fecha_consumo, monto) VALUES (2, 1, 'Compras D1', '2019-03-10', '$300.000');
+INSERT INTO CONSUMOS (id, tarjeta_id, descripcion, fecha_consumo, monto) VALUES (3, 1, 'Compras R', '2019-03-15', '$400.000');
+INSERT INTO CONSUMOS (id, tarjeta_id, descripcion, fecha_consumo, monto) VALUES (4, 1, 'Compras Claro', '2019-03-19', '$250.000');
+INSERT INTO CONSUMOS (id, tarjeta_id, descripcion, fecha_consumo, monto) VALUES (5, 2, 'Compras TV', '2019-02-05', '$700.000');
+
+INSERT INTO CONSUMOS (id, tarjeta_id, descripcion, fecha_consumo, monto) VALUES (6, 2, 'Compras IBM', '2019-02-28', '$850.000');
+INSERT INTO CONSUMOS (id, tarjeta_id, descripcion, fecha_consumo, monto) VALUES (7, 3, 'Compras Java', '2019-03-19', '$250.000');
+INSERT INTO CONSUMOS (id, tarjeta_id, descripcion, fecha_consumo, monto) VALUES (8, 3, 'Compras Angular', '2019-02-24', '$700.000');
+INSERT INTO CONSUMOS (id, tarjeta_id, descripcion, fecha_consumo, monto) VALUES (9, 3, 'Compras Payara', '2019-03-15', '$850.000');
 
 INSERT INTO ASESORES VALUES (1, 'Bancos', 'Jhonatan Restrepo');
 INSERT INTO ASESORES VALUES (2, 'Creditos', 'Andres Restrepo');
